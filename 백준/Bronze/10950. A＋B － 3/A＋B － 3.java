0772e99d1f4f33c7675325577bc1e32ff87f0ae7
C4ffe1ne <121875189+c4ffe1ne@users.noreply.github.com>
2023-01-15 19:54:59 +0900

    [Bronze V] Title: A+B - 3, Time: 140 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        int count = Integer.parseInt(reader.readLine());
+        for (int i = 0; i < count; i++) {
+            StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+            int argA = Integer.parseInt(tokenizer.nextToken());
+            int argB = Integer.parseInt(tokenizer.nextToken());
+            int calc = argA + argB;
+            writer.write(String.valueOf(calc));
+            writer.newLine();
+            writer.flush();
+        }
+    }
+}
